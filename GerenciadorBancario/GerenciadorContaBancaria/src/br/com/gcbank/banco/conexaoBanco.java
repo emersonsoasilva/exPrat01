87b401d1ad3b55915654aaf2850b441821251f30
emersonsoasilva <emersonsoares141@gmail.com>
2025-09-21 04:15:27 -0300

    update banco (não funfou)

--- GerenciadorBancario/GerenciadorContaBancaria/src/br/com/gcbank/banco/conexaoBanco.java
+++ GerenciadorBancario/GerenciadorContaBancaria/src/br/com/gcbank/banco/conexaoBanco.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class conexaoBanco {
-    private static final String URL = "jdbc:mysql://localhost:3306/banco_digital";
+    private static final String URL = "jdbc:mysql://localhost:3306/banco_digital?useSSL=false";
     private static final String USER = "root";
-    private static final String PASS = "Root1234@";
+    private static final String PASS = "Root1234#";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
